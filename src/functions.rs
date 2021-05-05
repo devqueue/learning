493604764648e30b803d0d71769ed7d2bdc8e545
Haziq Sayyed <haziq.sayyed@gmail.com>
2021-05-05 23:45:14 +0300

    functions added

---
+++ src/functions.rs
@@ -0,0 +1,23 @@
+pub fn run(){
+    greeting("hello", "Haziq");
+    
+    // Bind function values to variables
+    let getsum = add(5,5);
+    println!("Sum: {}", getsum);
+
+    // closure
+    let n3:i32 = 10;
+    let add_nums = | n1:i32, n2:i32 | n1+n2+n3;  // main advantage: can use global vars like n3
+    println!("Closure sum : {}", add_nums(3,3))
+
+}
+
+
+fn greeting(greet: &str, name: &str){
+    println!("{}, nice to meet you {}", greet, name )
+}
+
+
+fn add(n1: i32, n2: i32) -> i32 {
+    n1+n2 // No semi colon means this is what we will return 
+}
