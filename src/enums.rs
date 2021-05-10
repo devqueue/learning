1fc03162442d8223624668a8d3a0b009158193d5
Haziq Sayyed <haziq.sayyed@gmail.com>
2021-05-10 13:47:52 +0300

    pluralsight enums added

--- src/enums.rs
+++ src/enums.rs
@@ -6,6 +6,8 @@ enum Movement{
     Down,
     Left,
     Right,
+    Uplrmov(u8, u8, u8, u8),
+    Cmyk{cyan:u8, magenta: u8,  yellow:u8, black: u8}
 }
 
 fn move_avatar(m: Movement){
@@ -15,6 +17,10 @@ fn move_avatar(m: Movement){
         Movement::Down => println!("Avatar Moving Down"),
         Movement::Left => println!("Avatar Moving Left"),
         Movement::Right => println!("Avatar Moving Right"),
+        Movement::Uplrmov(0,0,0,0) 
+        | Movement::Cmyk{cyan:0, magenta: 0, yellow: 0, black: 0}=> println!("Dont move"), // movement 0 or colors 0 => print dont move
+        Movement::Uplrmov(u, p, l, r) => println!("Move {} {} {} {} respectively", u, p, l, r),
+        _ => () // anything else => do nothing
  
     }
 }
