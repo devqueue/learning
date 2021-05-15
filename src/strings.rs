b957409ca7530efa924646b8cb4c2dc29427f6f2
Haziq Sayyed <haziq.sayyed@gmail.com>
2021-05-15 22:01:04 +0300

    pluralsight course added

--- src/strings.rs
+++ src/strings.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 // Primitive str = Immutable fixed-length string somewhere in memory
 // String = Growable, heap-allocated data structure - Use when you need to modify or own string data
 
@@ -43,4 +45,37 @@ pub fn run(){
     // Assertion testing
     assert_eq!(2, s.len());
     assert_eq!(10, s.capacity());
+
+
+    // pluralsight 
+    let st:&'static str = "hello there"; // static is the life time of str
+    println!("{}", st);
+
+    // it is a utf 8 sequence of characters so you cannot index using st[0] because you're indexing into the ut8 sequence of bytes. but there is a way to get chars out of the string
+
+    for c in st.chars(){  //st.chars().rev()
+        println!("{}", c);
+    }
+
+    if let Some(first_char) = st.chars().nth(0){
+        println!("first char is {}", first_char);
+    }
+
+    // heap allocated string 
+    let mut letters = String::new();
+    let mut a = 'a' as u8;
+    while a <= ('z' as u8){
+        letters.push(a as char);
+        letters.push_str(",");
+        a+=1
+    }
+    println!("{}", letters);
+
+    // conversion between &str and String
+    let _u:&str = &letters;
+
+    let _abc = "hello".to_string();
+
+    
+
 }
