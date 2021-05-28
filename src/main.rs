5db1f9e1bae9fa1f8f4ec4c869fb04e22e2686d6
Haziq Sayyed <haziq.sayyed@gmail.com>
2021-05-28 13:52:22 +0300

    new mods added in main

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,14 @@
 // mod cli;
 // mod match_case;
 // mod optiont;
+// mod slices;
+// mod pattern_matching;
+// mod generics;
+// mod method;
+// mod closures;
+// mod higher_order_fn;
+// mod traits;
+mod ownership;
 
 
 // GLOBAL VARIABLE DECLARATION
@@ -48,7 +56,14 @@ fn main() {
     // cli::run();
     // match_case::run(7);
     // optiont::run()
-
+    // slices::run();
+    // pattern_matching::run();
+    // generics::run();
+    // method::run();
+    // closures::run();
+    // higher_order_fn::run();
+    // traits::run();
+    ownership::run();
     
 
     println!("MEANING OF LIFE {}", MEANING_OF_LIFE);
